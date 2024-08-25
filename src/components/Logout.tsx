75a58e08a02854fcfbf6f6cae9950c1e8ebab338
takashi <tks.srtr89@gmail.com>
2024-08-25 12:17:17 +0900

    feat: logout

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { sessionLogout } from '@/serverActions/sessionLogout'
+
 export const Logout = () => {
 	return (
 		<div className="m-4">
-			<button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+			<button
+				className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+				onClick={() => sessionLogout()}>
 				ログアウト
 			</button>
 		</div>
--- src/serverActions/sessionLogout/index.ts
+++ src/serverActions/sessionLogout/index.ts
@@ -1,30 +1,13 @@
 'use server'
 
-import { admninSdkAuth } from '@/utils/auth/adminSdk'
 import { clog } from '@/utils/log/node'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export async function sessionLogout(idToken: string) {
+export async function sessionLogout() {
 	clog.blue('>>>[sessionLogout]')
-	console.log('idToken: ', { idToken })
 
 	const cookieStore = cookies()
-	console.log({ cookieStore })
-	console.log('cookieStore.size:', cookieStore.size)
-	cookieStore.getAll().map((cookie) => console.log({ cookie }))
-
-	try {
-		/** Check id token is valid */
-		const verifiedIdToken = await admninSdkAuth.verifyIdToken(idToken).catch((error) => {
-			clog.red(error as string)
-			throw new Error('Invalid id token.')
-		})
-
-		console.log({ verifiedIdToken })
-	} catch (error) {
-		if (typeof error === 'string') {
-			clog.red(error as string)
-			throw Error(error)
-		}
-	}
+	cookieStore.delete('session')
+	redirect('/login')
 }
