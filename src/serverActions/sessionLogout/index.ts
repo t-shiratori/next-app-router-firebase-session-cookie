7761a188d34ece67fd61774a266947b70250e097
takashi <tks.srtr89@gmail.com>
2024-08-29 23:56:01 +0900

    update: logout

--- src/serverActions/sessionLogout/index.ts
+++ src/serverActions/sessionLogout/index.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { clog } from '@/utils/log/node'
+import { getAuth } from 'firebase-admin/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
@@ -8,6 +9,40 @@ export async function sessionLogout() {
 	clog.blue('>>>[sessionLogout]')
 
 	const cookieStore = cookies()
+
+	const sessionCookie = cookieStore.get('session')?.value
+
+	if (!sessionCookie) {
+		redirect('/login')
+	}
+
 	cookieStore.delete('session')
+
+	const decodedClaims = await getAuth()
+		.verifySessionCookie(sessionCookie)
+		.catch((error) => {
+			clog.red(error)
+		})
+
+	if (!decodedClaims) {
+		redirect('/login')
+	}
+
+	await getAuth()
+		.revokeRefreshTokens(decodedClaims.sub)
+		.catch((error) => {
+			clog.red(error)
+		})
+
 	redirect('/login')
+
+	// .then((decodedClaims) => {
+	// 	return getAuth().revokeRefreshTokens(decodedClaims.sub)
+	// })
+	// .then(() => {
+	// 	redirect('/login')
+	// })
+	// .catch((error) => {
+	// 	redirect('/login')
+	// })
 }
